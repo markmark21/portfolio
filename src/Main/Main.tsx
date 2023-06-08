ec9291bfd76f3eea35aab9ecc0af15e6278b985a
Mark Khramov <hramov.mark321@gmail.com>
2023-06-08 13:24:18 +0300

    main

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import s from "./Main.module.css";
+import styleContainer from '../common/styles/Container.module.css'
 
 export const Main = () => {
     return (
         <div className={s.mainBlock}>
-            <div className={s.container}>
+            <div className={styleContainer.container}>
                 <div className={s.greeting}>
                     <span>Hi There</span>
                     <h1>I am Mark Khramov</h1>
